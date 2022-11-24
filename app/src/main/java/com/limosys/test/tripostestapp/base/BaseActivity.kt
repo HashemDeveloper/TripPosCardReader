d7ac75aaf33936fac24af7ef4f8f3332776c39bf
Abul Hashem Jr <abul@limosys.com>
2022-11-24 13:08:55 -0500

    Shows toast message for now

--- app/src/main/java/com/limosys/test/tripostestapp/base/BaseActivity.kt
+++ app/src/main/java/com/limosys/test/tripostestapp/base/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.limosys.test.tripostestapp.base
 
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.vantiv.triposmobilesdk.Device
 import com.vantiv.triposmobilesdk.DeviceConnectionListener
@@ -8,22 +9,28 @@ import java.lang.Exception
 abstract class BaseActivity:ComponentActivity(), DeviceConnectionListener {
 
     override fun onConnected(p0: Device?, p1: String?, p2: String?, p3: String?) {
-        print("Device connected")
+        displayToastMessage("Device Connected")
     }
 
     override fun onDisconnected(p0: Device?) {
-        print("Disconnected")
+        displayToastMessage("Disconnected")
     }
 
     override fun onError(p0: Exception?) {
+        displayToastMessage(p0?.message ?: "")
         print(p0?.message)
     }
 
     override fun onBatteryLow() {
-        print("Low Battery")
+        displayToastMessage("Low Battery")
     }
 
     override fun onWarning(p0: Exception?) {
-        print(p0?.message)
+        displayToastMessage(p0?.message ?: "")
+    }
+    private fun displayToastMessage(message: String) {
+        runOnUiThread  {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
     }
 }
