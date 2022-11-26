e44de7f4a4c67f100405fcfcaac17c4f388ed804
Abul Hashem Jr <abul@limosys.com>
2022-11-26 01:34:31 -0500

    Clean up

--- app/src/main/java/com/limosys/test/tripostestapp/TriposTestMainActivity.kt
+++ app/src/main/java/com/limosys/test/tripostestapp/TriposTestMainActivity.kt
@@ -1,41 +1,21 @@
 package com.limosys.test.tripostestapp
 
-import android.content.Context
-import android.os.Build
+
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.MultiplePermissionsState
-import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.limosys.test.tripostestapp.base.BaseActivity
 import com.limosys.test.tripostestapp.ui.navigation.TriposNavigation
 import com.limosys.test.tripostestapp.ui.theme.TriposTestAppTheme
-import com.limosys.test.tripostestapp.utils.TriposConfig
-import com.vantiv.triposmobilesdk.VTP
-import com.vantiv.triposmobilesdk.triPOSMobileSDK
 import dagger.hilt.android.AndroidEntryPoint
 
-@OptIn(ExperimentalPermissionsApi::class)
 @AndroidEntryPoint
 class TriposTestMainActivity : BaseActivity() {
-    private lateinit var sharedVtp: VTP
     override fun onCreate(savedInstanceState: Bundle?) {
-        this.sharedVtp = triPOSMobileSDK.getSharedVtp()
         super.onCreate(savedInstanceState)
         setContent {
             AppEntryPoint()
--- app/src/main/java/com/limosys/test/tripostestapp/base/BaseActivity.kt
+++ app/src/main/java/com/limosys/test/tripostestapp/base/BaseActivity.kt
@@ -1,36 +1,5 @@
 package com.limosys.test.tripostestapp.base
 
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.vantiv.triposmobilesdk.Device
-import com.vantiv.triposmobilesdk.DeviceConnectionListener
-import java.lang.Exception
 
-abstract class BaseActivity:ComponentActivity(), DeviceConnectionListener {
-
-    override fun onConnected(p0: Device?, p1: String?, p2: String?, p3: String?) {
-        displayToastMessage("Device Connected")
-    }
-
-    override fun onDisconnected(p0: Device?) {
-        displayToastMessage("Disconnected")
-    }
-
-    override fun onError(p0: Exception?) {
-        displayToastMessage(p0?.message ?: "")
-        print(p0?.message)
-    }
-
-    override fun onBatteryLow() {
-        displayToastMessage("Low Battery")
-    }
-
-    override fun onWarning(p0: Exception?) {
-        displayToastMessage(p0?.message ?: "")
-    }
-    private fun displayToastMessage(message: String) {
-        runOnUiThread  {
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-        }
-    }
-}
+abstract class BaseActivity:ComponentActivity()
